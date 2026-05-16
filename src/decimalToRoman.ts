ff5f540875f3d7b217fb0ffb911b739445efa018
kawinwat <kawinwatleo48@gmail.com>
2026-05-16 13:14:51 +0700

    feat: decimal to roman function

---
+++ src/decimalToRoman.ts
@@ -0,0 +1,45 @@
+const decimal_values: [number, string][] = [
+  [1000, "M"],
+  [900, "CM"],
+  [500, "D"],
+  [400, "CD"],
+  [100, "C"],
+  [90, "XC"],
+  [50, "L"],
+  [40, "XL"],
+  [10, "X"],
+  [9, "IX"],
+  [5, "V"],
+  [4, "IV"],
+  [1, "I"],
+];
+
+function decimalToRoman(decimal: number): string {
+  let result: string = "";
+
+  for (const [value, symbol] of decimal_values) {
+    while (decimal >= value) {
+      result += symbol;
+      decimal -= value;
+    }
+  }
+
+  return result;
+}
+
+function main(): void {
+  const input = process.argv[2];
+
+  if (!input || !/^\d+$/.test(input)) {
+    console.log(
+      "Usage: node --experimental-strip-types src/decimalToRoman.ts <number>",
+    );
+    process.exit(1);
+  }
+
+  const decimal = parseInt(input, 10);
+
+  console.log(decimalToRoman(decimal));
+}
+
+main();
